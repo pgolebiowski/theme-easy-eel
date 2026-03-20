18325fa8a3cee7b52261477f5200c8e31f972da3
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2026-03-20 18:31:52 +0000

    Consolidate git functions into single file

--- functions/easy_eel/prompt_sections/git.fish
+++ functions/easy_eel/prompt_sections/git.fish
@@ -1,13 +1,12 @@
 source (dirname (realpath (status --current-filename)))/../colors.fish
-source (dirname (realpath (status --current-filename)))/git-helpers.fish
 
 function _easy_eel_display_prompt_section_git_state
-    not _git_is_repo; and return
+    not _easy_eel_git_is_repo; and return
 
     echo -n "on "
 
     set -q EASY_EEL_COLOR_GIT_BRANCH; or set -l EASY_EEL_COLOR_GIT_BRANCH purple
-    _easy_eel_colored_print $EASY_EEL_COLOR_GIT_BRANCH (_git_branch_name)
+    _easy_eel_colored_print $EASY_EEL_COLOR_GIT_BRANCH (_easy_eel_git_branch_name)
     echo -n ' '
 
     set -l state (_easy_eel_print_git_state)
@@ -23,22 +22,52 @@ function _easy_eel_print_git_state
 
     set -l result
 
-    if _git_is_touched
+    if _easy_eel_git_is_touched
         set result $EASY_EEL_SYMBOL_GIT_DIRTY
     else
         set result $EASY_EEL_SYMBOL_GIT_CLEAN
     end
 
-    if _git_has_untracked
+    if _easy_eel_git_has_untracked
         set result "$result$EASY_EEL_SYMBOL_GIT_UNTRACKED"
     end
 
-    if _git_is_stashed
+    if _easy_eel_git_is_stashed
         set result "$result$EASY_EEL_SYMBOL_GIT_STASH"
     end
 
-    set -l git_ahead_behind_diverged (_git_ahead_behind)
+    set -l git_ahead_behind_diverged (_easy_eel_git_ahead_behind)
     set result "$result$git_ahead_behind_diverged"
 
     echo $result
+end
+
+# Git helpers
+
+function _easy_eel_git_is_repo
+    git rev-parse --is-inside-work-tree &>/dev/null
+end
+
+function _easy_eel_git_branch_name
+    git symbolic-ref --short HEAD 2>/dev/null
+    or git rev-parse --short HEAD 2>/dev/null
+end
+
+function _easy_eel_git_is_touched
+    not git diff --quiet &>/dev/null
+    or not git diff --cached --quiet &>/dev/null
+end
+
+function _easy_eel_git_has_untracked
+    test -n "$(git ls-files --others --exclude-standard 2>/dev/null)"
+end
+
+function _easy_eel_git_is_stashed
+    git stash list 2>/dev/null | string length -q
+end
+
+function _easy_eel_git_ahead_behind
+    set -l s (git status -sb 2>/dev/null)
+    string match -rq 'ahead (\d+)' $s; and echo -n "↑$match[1]"
+    string match -rq 'behind (\d+)' $s; and echo -n "↓$match[1]"
 end
