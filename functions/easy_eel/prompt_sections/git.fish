a571e4570f93aedc1c5d60c4fdfbe9e8bf6770ae
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2026-03-20 19:16:44 +0000

    Speed up git status reporting

--- functions/easy_eel/prompt_sections/git.fish
+++ functions/easy_eel/prompt_sections/git.fish
@@ -1,73 +1,52 @@
-source (dirname (realpath (status --current-filename)))/../colors.fish
-
 function _easy_eel_display_prompt_section_git_state
-    not _easy_eel_git_is_repo; and return
+    # Gather git info
+    set -l info (git status --porcelain=v2 --branch 2>/dev/null)
+    or return
 
-    echo -n "on "
+    set -l branch ""
+    set -l dirty 0
+    set -l untracked 0
+    set -l ahead 0
+    set -l behind 0
+    set -l stashed 0
 
-    set -q EASY_EEL_COLOR_GIT_BRANCH; or set -l EASY_EEL_COLOR_GIT_BRANCH purple
-    _easy_eel_colored_print $EASY_EEL_COLOR_GIT_BRANCH (_easy_eel_git_branch_name)
-    echo -n ' '
+    for line in $info
+        switch $line
+            case '# branch.head *'
+                set branch (string sub -s 15 $line)
+            case '# branch.ab *'
+                string match -rq '\+[1-9]' $line; and set ahead 1
+                string match -rq ' -[1-9]' $line; and set behind 1
+            case '1 *' '2 *'
+                set dirty 1
+            case '\? *'
+                set untracked 1
+        end
+    end
 
-    set -l state (_easy_eel_print_git_state)
-    set -q EASY_EEL_COLOR_GIT_STATE; or set -l EASY_EEL_COLOR_GIT_STATE blue
-    _easy_eel_colored_print $EASY_EEL_COLOR_GIT_STATE "[$state]"
-end
+    test "$branch" = "(detached)"; and set branch (git rev-parse --short HEAD 2>/dev/null)
+    git rev-parse --verify --quiet refs/stash &>/dev/null; and set stashed 1
 
-function _easy_eel_print_git_state
+    # Symbols
     set -q EASY_EEL_SYMBOL_GIT_DIRTY; or set -l EASY_EEL_SYMBOL_GIT_DIRTY '!'
+    set -q EASY_EEL_SYMBOL_GIT_CLEAN; or set -l EASY_EEL_SYMBOL_GIT_CLEAN '✓'
     set -q EASY_EEL_SYMBOL_GIT_UNTRACKED; or set -l EASY_EEL_SYMBOL_GIT_UNTRACKED '☡'
     set -q EASY_EEL_SYMBOL_GIT_STASH; or set -l EASY_EEL_SYMBOL_GIT_STASH '↩'
-    set -q EASY_EEL_SYMBOL_GIT_CLEAN; or set -l EASY_EEL_SYMBOL_GIT_CLEAN '✓'
 
-    set -l result
+    # Colors
+    set -q EASY_EEL_COLOR_GIT_BRANCH; or set -l EASY_EEL_COLOR_GIT_BRANCH purple
+    set -q EASY_EEL_COLOR_GIT_STATE; or set -l EASY_EEL_COLOR_GIT_STATE blue
 
-    if _easy_eel_git_is_touched
-        set result $EASY_EEL_SYMBOL_GIT_DIRTY
-    else
-        set result $EASY_EEL_SYMBOL_GIT_CLEAN
-    end
+    # Build state
+    set -l state (test $dirty -eq 1; and echo $EASY_EEL_SYMBOL_GIT_DIRTY; or echo $EASY_EEL_SYMBOL_GIT_CLEAN)
+    test $untracked -eq 1; and set state "$state$EASY_EEL_SYMBOL_GIT_UNTRACKED"
+    test $ahead -eq 1; and set state "$state↑"
+    test $behind -eq 1; and set state "$state↓"
+    test $stashed -eq 1; and set state "$state$EASY_EEL_SYMBOL_GIT_STASH"
 
-    if _easy_eel_git_has_untracked
-        set result "$result$EASY_EEL_SYMBOL_GIT_UNTRACKED"
-    end
-
-    if _easy_eel_git_is_stashed
-        set result "$result$EASY_EEL_SYMBOL_GIT_STASH"
-    end
-
-    set -l git_ahead_behind_diverged (_easy_eel_git_ahead_behind)
-    set result "$result$git_ahead_behind_diverged"
-
-    echo $result
-end
-
-# Git helpers
-
-function _easy_eel_git_is_repo
-    git rev-parse --is-inside-work-tree &>/dev/null
-end
-
-function _easy_eel_git_branch_name
-    git symbolic-ref --short HEAD 2>/dev/null
-    or git rev-parse --short HEAD 2>/dev/null
-end
-
-function _easy_eel_git_is_touched
-    not git diff --quiet &>/dev/null
-    or not git diff --cached --quiet &>/dev/null
-end
-
-function _easy_eel_git_has_untracked
-    test -n "$(git ls-files --others --exclude-standard 2>/dev/null)"
-end
-
-function _easy_eel_git_is_stashed
-    git stash list 2>/dev/null | string length -q
-end
-
-function _easy_eel_git_ahead_behind
-    set -l s (git status -sb 2>/dev/null)
-    string match -rq 'ahead (\d+)' $s; and echo -n "↑$match[1]"
-    string match -rq 'behind (\d+)' $s; and echo -n "↓$match[1]"
+    # Display
+    echo -n "on "
+    set_color $EASY_EEL_COLOR_GIT_BRANCH; echo -n $branch; set_color normal
+    echo -n " "
+    set_color $EASY_EEL_COLOR_GIT_STATE; echo -n "[$state]"; set_color normal
 end
